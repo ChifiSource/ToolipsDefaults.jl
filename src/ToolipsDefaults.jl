7f2a851095b1e593ca94f415cac8e29e5c049583
emmy <52672675+emmettgb@users.noreply.github.com>
2022-11-10 02:37:50 -0600

    Add files via upload

--- src/ToolipsDefaults.jl
+++ src/ToolipsDefaults.jl
@@ -12,11 +12,23 @@ The ToolipsDefaults extension provides various default Styles and Components for
 """
 module ToolipsDefaults
 using Toolips
-import Toolips: SpoofConnection, AbstractComponent, div, AbstractConnection
+import Toolips: SpoofConnection, AbstractComponent, div, AbstractConnection, get
+import Toolips: Modifier
 using ToolipsSession
 
 include("Styles.jl")
 include("Components.jl")
+
+"""
+"""
+mutable struct RequestModifier <: Modifier
+    changes::Vector{String}
+end
+
+
+function get(f::Function, url::String)
+
+end
 
 export option, ColorScheme, dropdown, rangeslider, numberinput, containertextbox
 export textbox, pane, anypane, stylesheet, cursor, static_observer, numberinput
